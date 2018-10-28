187462c65120b748c89420e75dedf96423f28609
jaredgorski <jared.gorski@liferay.com>
2018-10-28 02:26:57 -0700

    Add basic prune command flow

--- main.sh
+++ main.sh
@@ -1,12 +1,38 @@
 #!/bin/bash
 
+source ./.env.sh
+source ./auth.sh
+source ./data.sh
+
 function prune() {
-  isrepo="$(git rev-parse --is-inside-work-tree 2>/dev/null)"
-  branches="$(git branch)"
-  if [ "$isrepo" ]
+  if [[ -v GITHUB_USER_NAME ]]
   then
-    echo $branches
+    isRepo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
+    
+    if [[ $1 ]]
+    then
+      handleArgs
+    else
+      if [[ $isRepo ]]
+      then
+        currentRepo=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
+        userRepos=$(listUserRepos)
+
+        if echo $userRepos | grep -w $currentRepo > /dev/null
+        then
+          fetchRemoteBranches "$currentRepo"
+        else
+          echo "Cannot resolve local repository with remote. Please verify that you own this remote repository: \"$currentRepo\""
+        fi
+      else
+        echo "This folder is not a repository"
+      fi
+    fi
   else
-    echo "not a repo"
+    login
   fi
+}
+
+function handleArgs() {
+  echo "argument: $1"
 }
