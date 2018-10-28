7e876f943c3ee88703b9ec9264ec4c72c104cb28
jaredgorski <jared.gorski@liferay.com>
2018-10-28 16:27:35 -0700

    Implement namespace, create basic command flow

--- main.sh
+++ main.sh
@@ -1,38 +1,98 @@
 #!/bin/bash
 
-source ./.env.sh
-source ./auth.sh
-source ./data.sh
-
 function prune() {
-  if [[ -v GITHUB_USER_NAME ]]
-  then
-    isRepo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
-    
-    if [[ $1 ]]
-    then
-      handleArgs
-    else
-      if [[ $isRepo ]]
-      then
-        currentRepo=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)\.git#\1#p')
-        userRepos=$(listUserRepos)
+  # Check whether current directory is a git repository
+  ghprune_isRepo="$(git rev-parse --is-inside-work-tree 2> /dev/null)"
 
-        if echo $userRepos | grep -w $currentRepo > /dev/null
-        then
-          fetchRemoteBranches "$currentRepo"
-        else
-          echo "Cannot resolve local repository with remote. Please verify that you own this remote repository: \"$currentRepo\""
-        fi
-      else
-        echo "This folder is not a repository"
-      fi
+  if [[ -z $1 ]] # Handle empty command
+  then
+    if [[ $ghprune_isRepo ]]
+    then
+      echo "\n\tERROR: No command received."
+      echo "\n\tusage: $0 [-i, -h, -s, -a, -r, -u, login, logout]"
+      echo "\n\tFor a list of commands and their uses, type \`prune --help\`\n"
+    else
+      echo "\n\tERROR: This directory is not a git repository. Please navigate to a git repository in order to use prune locally\n"
     fi
-  else
-    login
+  elif [[ $1 = "login" ]] || [[ $1 = "logout" ]] # Handle Github client authentication commands
+  then
+    if [[ $1 = "login" ]]
+    then
+      ghprune_handleLogin
+    elif [[ $1 = "logout" ]]
+    then
+      ghprune_handleLogout
+    fi
+  else # Handle unauthenticated commands
+    if [[ $ghprune_isRepo ]]
+    then
+      # Argument variables for local user
+      argsEnv=local
+      ghprune_interactiveFlag=off
+      ghprune_hardFlag=off
+      ghprune_softFlag=off
+      ghprune_allFlag=off
+      ghprune_remoteFlag=off
+
+      # Determine whether to handle arguments for local user
+      ghprune_willHandleLocalArgs=0
+
+      while [ $# -gt 0 ]
+      do
+        case "$1" in
+          -i) ghprune_interactiveFlag=on; ghprune_willHandleLocalArgs=1;;
+          -h) ghprune_hardFlag=on; ghprune_willHandleLocalArgs=1;;
+          -s) ghprune_softFlag=on; ghprune_willHandleLocalArgs=1;;
+          -a) ghprune_allFlag=on; ghprune_willHandleLocalArgs=1;;
+          -r) ghprune_remoteFlag=on; ghprune_willHandleLocalArgs=1;;
+          -u) ghprune_handleUser "$@"; ghprune_willHandleLocalArgs=0; break;;
+          --) shift; break;;
+          -*) echo >&2 "\n\tusage: $0 [-i, -h, -s, -a, -r, -u, login, logout]\n"
+              break;;
+          *)  break;;
+        esac
+        shift
+      done
+
+      if [[ $ghprune_willHandleLocalArgs -gt 0 ]]
+      then
+        ghprune_handleArgs "$argsEnv" "$ghprune_interactiveFlag" "$ghprune_hardFlag" "$ghprune_softFlag" "$ghprune_allFlag" "$ghprune_remoteFlag"
+      fi
+    else
+      echo "\n\tERROR: This directory is not a git repository. Please navigate to a git repository in order to use prune locally\n"
+    fi
   fi
 }
 
-function handleArgs() {
-  echo "argument: $1"
+function ghprune_handleArgs() {
+  # Under construction ---> localprune.sh?
+  echo "$@"
+}
+
+function ghprune_handleUser() {
+  # Argument variables
+  ghprune_currentRepo=
+
+  while [ $# -gt 0 ]
+  do
+    case "$2" in
+      -R) ghprune_currentRepo="$3"; shift;;
+      show) echo "\n\tCurrently logged in as: '$GHPRUNE_GITHUB_USER_NAME'\n";;
+      --) shift; break;;
+      -*) echo >&2 "\n\tusage: prune $1 [-R <repository>, show]\n"
+          break;;
+      *)  break;;
+    esac
+    shift
+  done
+}
+
+function ghprune_handleLogin() {
+  source ./auth.sh
+  ghprune_userLogin
+}
+
+function ghprune_handleLogout() {
+  source ./auth.sh
+  ghprune_userLogout
 }
