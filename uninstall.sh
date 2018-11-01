1ad9bec6c3091cab5f02bc599cdaf6ef10140c07
Jared Gorski <jared.gorski@liferay.com>
2018-11-01 03:01:47 -0700

    Add installer and uninstaller

---
+++ uninstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -n pruneDir ]]
+then
+  ghpruneExec="[ -s \"$HOME/.gh_prune/gh_prune.sh\" ] && source \"$HOME/.gh_prune/gh_prune.sh\""
+
+  for profile in bashrc zshrc bash_profile
+  do
+    if [[ -s "$HOME/.$profile" ]]
+    then
+      if grep -q "$ghpruneExec" "$HOME/.$profile"
+      then
+        awk '!/#KEEP_THIS_COMMENT-AddsPruneToShell/' "$HOME/.$profile" > .temprc && mv .temprc "$HOME/.$profile"
+        rm -rf "$HOME/.gh_prune"
+        printf "\n\tPrune uninstalled successfully. Restart your shell.\n\n"
+      else
+        printf "\n\tPrune is not currently installed on your profile. Execute \`./install.sh\` if you would like to install.\n"
+        printf "\n\tExecute \`./install.sh\` if you would like to install.\n\n"
+      fi
+    fi
+  done
+else
+  printf "\n\tPrune is not currently installed on your profile. Execute \`./install.sh\` if you would like to install.\n"
+  printf "\n\tExecute \`./install.sh\` if you would like to install.\n\n"
+fi
+
