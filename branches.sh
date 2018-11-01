e113c3293b4cf12e53768c68a77d9ef0e22c6bfc
Jared Gorski <jared.gorski@liferay.com>
2018-11-01 03:00:58 -0700

    Improve branches code, remove gnu-sed dependency

--- branches.sh
+++ branches.sh
@@ -30,18 +30,17 @@ function ghprune_fetchPrunableBranches() {
 function ghprune_fetchLocalBranches() {
   ghprune_fetchedLocalBranches=()
 
-  IFS=$'\n'
+  local IFS=$'\n'
 
   ghprune_localBranches_raw=( $(git branch --no-color) )
 
   for item in ${ghprune_localBranches_raw[@]}
   do
     ghprune_localBranchName_untrimmed=${item##*\]}
+    ghprune_localBranchName_untrimmed=${item##*\*}
     ghprune_localBranchName=$(echo -e "${ghprune_localBranchName_untrimmed}" | tr -d '[:space:]')
     ghprune_fetchedLocalBranches+=($ghprune_localBranchName)
   done
-
-  unset IFS
 
   echo ${ghprune_fetchedLocalBranches[@]}
 }
@@ -49,21 +48,19 @@ function ghprune_fetchLocalBranches() {
 function ghprune_fetchRemoteBranches() {
   ghprune_fetchedRemoteBranches=()
 
-  IFS=$'\n'
+  local IFS=$'\n'
 
   ghprune_remoteBranches_raw=( $(git branch -r --no-color) )
 
   for item in ${ghprune_remoteBranches_raw[@]}
   do
-    if [ ! $item =~ "HEAD" ] && [ ! $item =~ "->" ]
+    if [[ ! $item =~ "HEAD" ]] && [[ ! $item =~ "->" ]]
     then
       ghprune_remoteBranchName_untrimmed=${item##*/}
       ghprune_remoteBranchName=$(echo -e "${ghprune_remoteBranchName_untrimmed}" | tr -d '[:space:]')
       ghprune_fetchedRemoteBranches+=($ghprune_remoteBranchName)
     fi
   done
-
-  unset IFS
 
   echo ${ghprune_fetchedRemoteBranches[@]}
 }
